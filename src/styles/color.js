889cf04692251e8e6d00e7b7016b7c6cdfec0f37
YeonjuSeo <tjduswn1219@gmail.com>
2021-11-20 04:27:41 +0900

    📝  typograpy, font 체계 작성

---
+++ src/styles/color.js
@@ -0,0 +1,6 @@
+export const PRIMARY = "#985DE3";
+export const GRAY4 = "#525252";
+export const GRAY3 = "#696969";
+export const GRAY2 = "#8d8d8d";
+export const GRAY1 = "#8C8C8C";
+export const WHITE = "#ffffff";
---
+++ src/styles/typography.js
@@ -0,0 +1,57 @@
+import { css } from "styled-components/native";
+
+export const Bold16 = css`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19.2px;
+`;
+export const Bold14 = css`
+    font-weightL700;
+    font-size:14px;
+    line-height:17px;
+`;
+export const SemiBold17 = css`
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 22px;
+`;
+export const SemiBold16 = css`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 19px;
+`;
+export const SemiBold14 = css`
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 17px;
+`;
+export const Medium15 = css`
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 18px;
+`;
+export const Medium14 = css`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 17px;
+`;
+export const Medium13 = css`
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 16px;
+`;
+export const Medium12 = css`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 14px;
+`;
+export const Regular14 = css`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+`;
+export const Regular12 = css`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+`;
