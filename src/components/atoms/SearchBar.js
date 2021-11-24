96141f73fbdeaabf2f30ef58db209f30764fac19
YeonjuSeo <tjduswn1219@gmail.com>
2021-11-25 05:11:20 +0900

    💄 SearchBar 파일 추가

---
+++ src/components/atoms/SearchBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Image, TouchableOpacity } from "react-native";
+import { SearchBar } from "react-native-elements";
+import SearchIcon from "../../assets/search.png";
+
+export function SearchButton({ navigation }) {
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate("Result");
+      }}
+    >
+      <Image source={SearchIcon} style={{ width: 26, height: 26 }} />
+    </TouchableOpacity>
+  );
+}
+
+export default function HomeSearchBar({ wishAddr, navigation }) {
+  return (
+    <SearchBar
+      placeholder={`${wishAddr}의 어떤 집을 찾고 계세요?`}
+      containerStyle={{
+        backgroundColor: "white",
+        marginTop: 18,
+        marginBottom: 20,
+        // height: 50,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.05,
+        shadowRadius: 10,
+        border: "none",
+      }}
+      inputContainerStyle={{ backgroundColor: "white" }}
+      searchIcon={<SearchButton navigation={navigation} />}
+    />
+  );
+}
+
+// const SearchBar = styled.TextInput`
+//   background-color: white;
+//   border: 1px solid #ededed;
+//   height: 50px;
+//   box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.05);
+//   margin: 18px 0 20px 0;
+//   padding: 0 9px;
+// `;
