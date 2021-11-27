87a76da21e773654bf84869364a05d7489ef2487
YeonjuSeo <tjduswn1219@gmail.com>
2021-11-27 14:41:22 +0900

    💄  splash screen 디자인 추가 반영

--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -3,11 +3,23 @@ import styled from "styled-components/native";
 import { Image, Text, View } from "react-native";
 
 import LogoImg from "../../assets/FullLogoImg.png";
+import bgImg1 from "../../assets/splashBG1.png";
+import bgImg2 from "../../assets/splashBG2.png";
+import bgImg3 from "../../assets/splashBG3.png";
 
 export default function SplashScreen() {
   return (
     <Wrapper>
       <Image source={LogoImg} />
+      <Image style={{ position: "absolute", bottom: 0 }} source={bgImg1} />
+      <Image
+        style={{ position: "absolute", bottom: 0, left: 0 }}
+        source={bgImg2}
+      />
+      <Image
+        style={{ position: "absolute", top: 80, right: 0 }}
+        source={bgImg3}
+      />
     </Wrapper>
   );
 }
