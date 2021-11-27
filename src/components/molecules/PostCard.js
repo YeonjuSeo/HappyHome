794423f87097cd51fcf3a32983adafcb72ce870c
YeonjuSeo <tjduswn1219@gmail.com>
2021-11-27 19:11:30 +0900

    💄  disabled 디자인 반영

--- src/components/molecules/PostCard.js
+++ src/components/molecules/PostCard.js
@@ -23,22 +23,22 @@ export default function PostCard({ navigation, post }) {
       }}
     >
       <LeftContainer>
-        <Title numberOfLines={2} ellipsizeMode="tail">
+        <Title numberOfLines={2} ellipsizeMode="tail" isDealt={post.isDealt}>
           {post.title}
         </Title>
         <SubTitleWrapper>
-          <SubTitle>
+          <SubTitle isDealt={post.isDealt}>
             {post.location.split(" ")[1]} {post.location.split(" ")[2]}
           </SubTitle>
-          <SubTitle> · </SubTitle>
-          <SubTitle>
+          <SubTitle isDealt={post.isDealt}> · </SubTitle>
+          <SubTitle isDealt={post.isDealt}>
             {new Date(post.createdAt * 1000).getMonth() + 1}월{" "}
             {new Date(post.createdAt * 1000).getDate()}일
           </SubTitle>
         </SubTitleWrapper>
 
-        <Distance>{post.distance}m</Distance>
-        <Price>₩ {post.rentalFeeWeek}0,000/주 </Price>
+        <Distance isDealt={post.isDealt}>{post.distance}m</Distance>
+        <Price isDealt={post.isDealt}>₩ {post.rentalFeeWeek}0,000/주 </Price>
       </LeftContainer>
 
       <StyledImg
@@ -57,13 +57,14 @@ const Wrapper = styled.TouchableOpacity`
   border-bottom-color: #e6e6e6;
   border-bottom-width: 1px;
   justify-content: space-between;
-  background-color: ${(props) => (props.isDealt ? `${GRAY0}` : "white")};
+  background-color: white;
 `;
 const LeftContainer = styled.View`
   width: 169px;
 `;
 const Title = styled.Text`
   ${SemiBold17};
+  color: ${(props) => (props.isDealt ? "#b7b7b7" : "black")};
 `;
 const SubTitleWrapper = styled.View`
   display: flex;
@@ -72,15 +73,18 @@ const SubTitleWrapper = styled.View`
 const SubTitle = styled.Text`
   ${Medium12};
   margin: 8px 0 6px 0;
+  color: ${(props) => (props.isDealt ? "#b7b7b7" : "black")};
 `;
 const Distance = styled.Text`
   ${Medium12};
   color: ${GRAY2};
+  color: ${(props) => (props.isDealt ? "#b7b7b7" : "black")};
 `;
 const Price = styled.Text`
   ${Bold16};
   color: ${PRIMARY};
   margin-top: 37px;
+  color: ${(props) => (props.isDealt ? "#b7b7b7" : "black")};
 `;
 const StyledImg = styled.Image`
   width: 144px;
